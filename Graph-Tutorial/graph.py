560cfdcd1d12b190f4c4c1a3307f430875699c67
Drew McGowan <michael.mcgowan@students.makeschool.com>
2019-07-15 12:11:42 -0700

    graph tutorial done

--- Graph-Tutorial/graph.py
+++ Graph-Tutorial/graph.py
@@ -19,17 +19,23 @@ class Vertex(object):
 
     def addNeighbor(self, vertex, weight=0):
         """add a neighbor along a weighted edge"""
-        # TODO check if vertex is already a neighbot
-        # TODO if not, add vertex to neighbors and assign weight.
+        #  check if vertex is already a neighbor
+        if vertex in self.neighbors:
+            raise ValueError('Item exists in neighbor: {}'.format(vertex))
+        #  if not, add vertex to neighbors and assign weight.
+        else:
+            self.neighbors[vertex] = weight
+            # return self.neighbors
+
 
     def __str__(self):
         """output the list of neighbors of this vertex"""
-        return str(self.id) + " adjancent to " +
-        str([x.id for x in self.neighbors])
+        return str(self.id) + " adjancent to " + str([x.id for x in self.neighbors])
 
     def getNeighbors(self):
         """return the neighbors of this vertex"""
-        # TODO return the neighbors
+        #  return the neighbors
+        return self.neighbors.keys()
 
     def getId(self):
         """return the id of this vertex"""
@@ -37,8 +43,8 @@ class Vertex(object):
 
     def getEdgeWeight(self, vertex):
         """return the weight of this edge"""
-        # TODO return the weight of the edge from this
-        vertext to the given vertex.
+        #  return the weight of the edge from this vertex to the given vertex.
+        return self.neighbors.values()
 
 
 """ Graph Class
@@ -51,41 +57,54 @@ class Graph:
     def __init__(self):
         """ initializes a graph object with an empty dictionary.
         """
-        self.vertList = {}
+        self.vert_dict = {}
         self.numVertices = 0
 
     def addVertex(self, key):
         """add a new vertex object to the graph with
         the given key and return the vertex
         """
-        # TODO increment the number of vertices
-        # TODO create a new vertex
-        # TODO add the new vertex to the vertex list
-        # TODO return the new vertex
+        #  increment the number of vertices
+        self.numVertices += 1
+        #  create a new vertex
+        new_vertex = Vertex(key)
+        #  add the new vertex to the vertex list
+        self.vert_dict[key] = new_vertex
+        #  return the new vertex
+        return self
 
     def getVertex(self, n):
         """return the vertex if it exists"""
-        # TODO return the vertex if it is in the graph
+        #  return the vertex if it is in the graph
+        if n in self.vert_dict:
+            return n
+        raise ValueError('Vertex not found: {}'.format(n))
 
     def addEdge(self, f, t, cost=0):
         """add an edge from vertex f to vertex t with a cost
         """
-        # TODO if either vertex is not in the graph,
+        # if either vertex is not in the graph,
         # add it - or return an error (choice is up to you).
-        # TODO if both vertices in the graph, add the
-        # edge by making t a neighbor of f
-        # and using the addNeighbor method of the Vertex class.
-        # Hint: the vertex f is stored in self.vertList[f].
+        if f not in self.vert_dict or t not in self.vert_dict:
+            raise ValueError('One of the vertices not in graph')
+            # if both vertices in the graph, add the
+            # edge by making t a neighbor of f
+        else:
+            # and using the addNeighbor method of the Vertex class.
+            # Hint: the vertex f is stored in self.vertList[f].
+            self.vert_dict[f].addNeighbor(self.vert_dict[t])
+
 
     def getVertices(self):
         """return all the vertices in the graph"""
-        return self.vertList.keys()
+        # return self.vertList.keys()
+        return self.vert_dict.keys()
 
     def __iter__(self):
         """iterate over the vertex objects in the
         graph, to use sytax: for v in g
         """
-        return iter(self.vertList.values())
+        return iter(self.vert_dict.values())
 
 
 # Driver code
@@ -101,13 +120,26 @@ if __name__ == "__main__":
     g.addVertex("Friend 1")
     g.addVertex("Friend 2")
     g.addVertex("Friend 3")
+    g.addVertex("Friend 4")
+    g.addVertex("Friend 5")
+    g.addVertex("Friend 6")
+    g.addVertex("Friend 7")
+    g.addVertex("Friend 8")
+    g.addVertex("Friend 9")
+    g.addVertex("Friend 10")
 
     # ...  add all 10 including you ...
 
     # Add connections (non weighted edges for now)
     g.addEdge("Friend 1", "Friend 2")
     g.addEdge("Friend 2", "Friend 3")
-
+    g.addEdge("Friend 3", "Friend 4")
+    g.addEdge("Friend 4", "Friend 5")
+    g.addEdge("Friend 5", "Friend 6")
+    g.addEdge("Friend 7", "Friend 8")
+    g.addEdge("Friend 8", "Friend 9")
+    g.addEdge("Friend 9", "Friend 10")
+    
     # Challenge 1: Output the vertices & edges
     # Print vertices
     print("The vertices are: ", g.getVertices(), "\n")
